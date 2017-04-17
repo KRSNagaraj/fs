7d6123d225c9c051c5a3cbc6a04da00abf6fa751
S Nagaraj <fusionraj@gmail.com>
2017-04-17 09:13:09 +0530

    menu working on
    menu working on

--- fs/App_Start/BundleConfig.cs
+++ fs/App_Start/BundleConfig.cs
@@ -23,9 +23,46 @@ namespace fs
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+            bundles.Add(new ScriptBundle("~/bundles/fs").Include(
+                  "~/Content/js/jquery-1.11.1.js",
+                  "~/Content/js/modernizr-2.6.2.min.js",
+                  "~/Content/vendor/bootstrap/js/bootstrap.min.js",
+                  "~/Content/vendor/rs-plugin/js/jquery.themepunch.plugins.min.js",
+                  "~/Content/vendor/rs-plugin/js/jquery.themepunch.revolution.js",
+                  "~/Content/js/jquery.shuffle.min.js",
+                  "~/Content/vendor/mmenu/js/jquery.mmenu.min.js",
+                  "~/Content/vendor/owl/js/owl.carousel.min.js",
+                  "~/Content/js/wow.min.js",
+                  //"http://cdnjs.cloudflare.com/ajax/libs/waypoints/2.0.3/waypoints.min.js",
+                  "~/Content/js/jquery.counterup.min.js",
+                  "~/Content/js/jquery.easing.min.js",
+                  "~/Content/js/scrolling-nav.js",
+                  "~/Content/js/smoothscroll.min.js",
+                  "~/Content/js/script.js",
+                  "~/Content/js/email.js"));
+
+
+            bundles.Add(new StyleBundle("~/Content/fs").Include(
+                      "~/Content/vendor/rs-plugin/css/settings.css",
+                      "~/Content/vendor/rs-plugin/css/extralayer.css",
+                      "~/Content/vendor/flat-icon/flaticon.css",
+                      "~/Content/vendor/font-awesome/css/font-awesome.min.css",
+                      "~/Content/vendor/owl/css/owl.carousel.css",
+                      "~/Content/vendor/owl/css/owl.theme.default.css",
+                      "~/Content/vendor/owl/css/owl.theme.css",
+                      "~/Content/vendor/mmenu/css/jquery.mmenu.css",
+                      "~/Content/vendor/bootstrap/css/bootstrap.css",
+                      "~/Content/css/animate.css",
+                      "~/Content/css/hover.css",
+                      "~/Content/css/style.css",
+                      "~/Content/css/responsive.css",
+
                       "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/Content/css-ori").Include(
+          "~/Content/bootstrap.css",
+          "~/Content/site.css"));
+
         }
     }
 }
